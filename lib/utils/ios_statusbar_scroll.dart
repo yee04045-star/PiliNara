b25c1879e3f392b064a00b3775100431e8100ce1
yee04045-star <yee04045@gmail.com>
2026-09-03 22:55:15 +0800

    Add files via upload

--- lib/utils/ios_statusbar_scroll.dart
+++ lib/utils/ios_statusbar_scroll.dart
@@ -1,24 +1,13 @@
-
-import 'dart:io';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 
-/// Bridges iOS status-bar taps to Flutter scroll views.
-abstract final class IOSStatusBarScroll {
-  static const _channel = MethodChannel('pilinara/ios_statusbar');
+class IOSStatusBarScroll {
+  static final channel = MethodChannel('piliplus/statusbar');
+  static VoidCallback? onTap;
 
   static void init() {
-    if (!Platform.isIOS) return;
-    _channel.setMethodCallHandler((call) async {
+    channel.setMethodCallHandler((call) async {
       if (call.method == 'scrollToTop') {
-        WidgetsBinding.instance.handlePopRoute();
-        await Scrollable.ensureVisible(
-          PrimaryScrollController.of(
-            WidgetsBinding.instance.rootElement!,
-          ).context!,
-          duration: const Duration(milliseconds: 450),
-          alignment: 0,
-        );
+        onTap?.call();
       }
     });
   }
