b7d3a6105ad5c11e35380fcf46b5520ecb066562
yee04045-star <yee04045@gmail.com>
2026-09-03 22:59:52 +0800

    Add files via upload

--- lib/utils/scroll_to_top_registry.dart
+++ lib/utils/scroll_to_top_registry.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/widgets.dart';
 
+/// Global registry for iOS status-bar tap -> scroll to top.
 class ScrollToTopRegistry {
   static final Map<String, ScrollController> _controllers = {};
 
-  static void register(String id, ScrollController controller) {
-    _controllers[id] = controller;
+  static void register(String key, ScrollController controller) {
+    _controllers[key] = controller;
   }
 
-  static void unregister(String id) {
-    _controllers.remove(id);
+  static void unregister(String key) {
+    _controllers.remove(key);
   }
 
-  static Future<void> scrollAllToTop() async {
+  static void scrollToTop() {
     for (final controller in _controllers.values) {
       if (!controller.hasClients) continue;
-      await controller.animateTo(
+      controller.animateTo(
         0,
         duration: const Duration(milliseconds: 380),
         curve: Curves.easeOutCubic,
